a5a85b94b28f51875edb30e3e4ac3c6320dbe019
Nelson Campos <ncamposfh@gmail.com>
2018-09-30 19:49:27 -0300

    end calc

--- RestAspNet - 01 API Calculator/RestAspNet/Controllers/CalculatorController.cs
+++ RestAspNet - 01 API Calculator/RestAspNet/Controllers/CalculatorController.cs
@@ -12,39 +12,60 @@ namespace RestAspNet.Controllers
     {
        
 
-        // GET api/values/5
-        [HttpGet("{firstNumber}/{secondNUmber}")]
+        // GET api/calculator/operacao/fistNumber/secondNumber
+        [HttpGet("soma/{firstNumber}/{secondNUmber}")]
         public IActionResult Sum(string firstNumber, string secondNumber)
         {
-
-            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
-            {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber)) {
                 var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
                 return Ok(sum.ToString());
             }
+            return BadRequest("Invalid Input");
+        }
 
+        // GET api/calculator/fistNumber/secondNumber
+        [HttpGet("subtracao/{firstNumber}/{secondNUmber}")]
+        public IActionResult Sub(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        // GET api/calculator/operacao/fistNumber/secondNumber
+        [HttpGet("divisao/{firstNumber}/{secondNUmber}")]
+        public IActionResult Div(string firstNumber, string secondNumber)
+        {
+
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) % ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
             return BadRequest("Invalid Input");
 
         }
 
-        private decimal ConvertToDecimal(string number)
-        {
-            decimal decimalValue;
 
+        /*
+            Métodos de Validação de Numerais
+        */
+
+        private decimal ConvertToDecimal(string number) {
+            decimal decimalValue;
             if(decimal.TryParse(number, out decimalValue))
             {
                 return decimalValue;
             }
-
             return 0;
         }
 
-        private bool IsNumeric(string strNumber)
-        {
+        private bool IsNumeric(string strNumber){
             double number;
-
                 bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
-
             return isNumber;
         }
     }
